9c3ea98eecdfe42243840a774dfaeaeba4ad39a3
Yingqi Chen <chanwk18@hotmail.com>
2019-02-19 17:07:11 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
 var element = document.createElement('div');//this file is optional
 //you can use this to paste in js code and have it run when index.html is loaded
-element.innerHTML = 'Hello, DOM!';
+element.innerHTML = 'Hello, World!';
 element.style.backgroundColor = '#f9f9f9';
